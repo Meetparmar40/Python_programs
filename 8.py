0777f5cc51094696a2796d01be26b43cf8d8bf23
Meetparmar40 <149680581+Meetparmar40@users.noreply.github.com>
2024-08-18 19:35:42 +0530

    Delete 8.py

--- 8.py
+++
@@ -1,7 +0,0 @@
-str = input("Enter the string you want to check : ")
-
-if str.isidentifier():
-    print("Given string is a valid identifier")
-else:
-    print("Given string is not a valid identifier")
-
